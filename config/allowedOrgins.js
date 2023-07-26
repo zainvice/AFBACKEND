9ed54727d1470876c0bc40f4986cef8cfae4fc08
zainvice <amazwings@gmail.com>
2023-07-26 21:49:34 +0500

    Cors Update

--- config/allowedOrgins.js
+++ config/allowedOrgins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3500',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://afproposals.onrender.com',
+    'https://www.afproposals.onrender.com'
 ]
 
 module.exports = allowedOrigins

389bb0ca015823dc689c0d1f9c207dd99c8f7026
zainvice <amazwings@gmail.com>
2023-08-07 20:54:04 +0500

    Name Update

--- controllers/draftController.js
+++ controllers/draftController.js
@@ -320,7 +320,7 @@ const updatedraft = asycHandler(async (req, res) =>{
                 console.log(links)
                 sendEmail(
                     foundUser.email,
-                    "Alphabet Factory | Tailored Business Proposal Inside!",
+                    "Your Tailored Business Proposal Inside!",
                     {
                       name: foundUser.fullName,
                       link: links,
